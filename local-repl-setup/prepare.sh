1edf4cef855380c966c1a877976afea06e370001
Dávid Horváth <horvath@webarticum.hu>
2025-07-26 14:08:32 +0200

    Clean prepare process in local-repl-setup

--- local-repl-setup/prepare.sh
+++ local-repl-setup/prepare.sh
@@ -5,9 +5,7 @@ workspaceDir="${selfDir}/workspace"
 
 rm -rf "${workspaceDir}/"  &&
 cp -r "${selfDir}/sample" "$workspaceDir" &&
-git clone -b develop 'https://github.com/miniconnect/holodb' "${workspaceDir}/holodb" && # FIXME: develop?
+git clone -b develop 'https://github.com/miniconnect/holodb' "${workspaceDir}/holodb" &&
 ( cd "${workspaceDir}/holodb" && ./gradlew config:generateSchema ) &&
 
-# FIXME: should this be handled in the config project?
-#cp "${workspaceDir}/holodb/projects/config/build/schemas/holodb-config.schema.json" "${workspaceDir}/config.schema.json" &&
 jq 'walk(if type == "object" and has("valuesBundle") then .valuesBundle += {enum: '"$( echo "${workspaceDir}/holodb/projects/values/src/main/resources/hu/webarticum/holodb/values/"* | tr ' ' '\n' | sed -E 's/(^.*\/|\.txt$)//g' | jq  -R -s -c 'split("\n") | map(select(length > 0))' )"'} else . end)' "${workspaceDir}/holodb/projects/config/build/schemas/holodb-config.schema.json" > "${workspaceDir}/config.schema.json"
